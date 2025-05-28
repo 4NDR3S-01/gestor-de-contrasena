ed52abe2e752bbae5489b6a3185dd8f3ccdbdbfb
Andres <williamcabrera20@hotmail.com>
2025-05-28 10:01:31 -0500

    Eliminar componentes y servicios de gestión de contraseñas

--- frontend/src/services/apiContrasenas.ts
+++
@@ -1,6 +0,0 @@
-import clienteAxios from "./api";
-
-export const crearContrasena = async (datos: any) => {
-  const respuesta = await clienteAxios.post("/contrasenas", datos);
-  return respuesta.data;
-};
